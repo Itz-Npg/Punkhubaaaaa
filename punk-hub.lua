2c471d1963353f7b0263bc1d3902f219cc506920
Itz-Npg <152873138+Itz-Npg@users.noreply.github.com>
2024-02-25 18:56:43 +0545

    Update punk-hub.lua

--- punk-hub.lua
+++ punk-hub.lua
@@ -10,6 +10,40 @@ local Window = PunkLib:CreateWindow({
     Theme = "Dark",
     MinimizeKey = Enum.KeyCode.Delete 
 })
+
+if game.CoreGui:FindFirstChild("PunkHubXOpenClose") then
+    game.CoreGui:FindFirstChild("PunkHubXOpenClose"):Destroy()
+end
+
+local UIStroke = Instance.new("UIStroke")
+local UICorner = Instance.new("UICorner")
+
+local ScreenGui = Instance.new("ScreenGui")
+local ImageButton = Instance.new("ImageButton")
+
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Name = "PunkHubXOpenClose"
+ScreenGui.Parent = game.CoreGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+
+ImageButton.Parent = ScreenGui
+ImageButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+ImageButton.BorderColor3 = Color3.fromRGB(255, 0, 0)
+ImageButton.Draggable = true
+ImageButton.Position = UDim2.new(0.102097899, 0, 0.0742971897, 0)
+ImageButton.Size = UDim2.new(0, 59, 0, 49)
+ImageButton.Image = "rbxassetid://16520492616"
+
+UICorner.Name = "MainCorner"
+UICorner.CornerRadius = UDim.new(0, 9)
+UICorner.Parent = ImageButton
+
+ImageButton.MouseButton1Down:connect(function()
+game:GetService("VirtualInputManager"):SendKeyEvent(true,127,false,game)
+game:GetService("VirtualInputManager"):SendKeyEvent(false,127,false,game)
+end)
+
 Home_Main = Window:AddTab({ Title = "Home", Icon = "scan-face" })
 Home_Main:AddButton({Title = "Join Discord - PunkTeam",Description = "Invite Discord PunkTeam!",Callback = function()
 	setclipboard("https://discord.gg/silentcaliber")
